7e4c460cf510b89df2f8f327607336f73ff85602
delia <ailed_249@hotmail.com>
2014-10-07 18:12:12 -0400

    tercer commit creacion de los modelos

--- juego/juego/juego/apps/usuario/models.py
+++ juego/juego/juego/apps/usuario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class jugador (models.Model):
+	nombre=models.CharField(max_length=200)
+	apellidos=models.CharField(max_length=200)
+	direccion=models.CharField(max_length=200)
+	telefono=models.IntegerFiel()
+	email=models.EmailField(max_length=75)
+	ci=models.IntegerFiel(unique=True)
+	fecha=models.DateTimeField(auto_now=True)
+	def __unicode__(self):
+		return "%s "%(self.nombre) 
+
